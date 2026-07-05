15652e144095e09e0ef8973799123e04b2a1bf06
Nechirvan <nechir.ibrahim@yahoo.com>
2026-07-05 12:56:40 +0100

    Adding Service Class

--- src/main/java/com/dastan/githubapp/GitController.java
+++ src/main/java/com/dastan/githubapp/GitController.java
@@ -1,13 +1,18 @@
 
 package com.dastan.githubapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GitController {
+    @Autowired
+    GitService gitService;
+    
     @GetMapping("/")
     public String hello(){
-        return "Hello, world!";
+        
+        return gitService.gitData();
     }
 }
---
+++ src/main/java/com/dastan/githubapp/GitService.java
@@ -0,0 +1,8 @@
+
+package com.dastan.githubapp;
+
+public class GitService {
+    public String gitData(){
+        return "git push origin main";
+    }
+}

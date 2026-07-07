a7523ad6002bf91196aa71710754227f7f14d16f
Nechirvan <nechir.ibrahim@yahoo.com>
2026-07-07 14:22:54 +0300

    Adding PathVaribale and Service Annotation

--- src/main/java/com/dastan/githubapp/GitController.java
+++ src/main/java/com/dastan/githubapp/GitController.java
@@ -3,6 +3,8 @@ package com.dastan.githubapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +14,10 @@ public class GitController {
     
     @GetMapping("/")
     public String hello(){
-        
         return gitService.gitData();
+    }
+    @GetMapping("/hi/{greet}")
+    public String greet(@PathVariable String greet){
+        return "Hello " + greet;
     }
 }
--- src/main/java/com/dastan/githubapp/GitService.java
+++ src/main/java/com/dastan/githubapp/GitService.java
@@ -1,7 +1,11 @@
 
 package com.dastan.githubapp;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class GitService {
+    
     public String gitData(){
         return "git push origin main";
     }

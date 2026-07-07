8e0746654f6961f47919ea6264a7df2344df4d21
Nechirvan <nechir.ibrahim@yahoo.com>
2026-07-07 18:14:37 +0300

    Adding feature2 branch

--- src/main/java/com/dastan/githubapp/Feature1.java
+++
@@ -1,6 +0,0 @@
-
-package com.dastan.githubapp;
-
-public class Feature1 {
-    //Adding new featue to the project for branching
-}
---
+++ src/main/java/com/dastan/githubapp/Feature2.java
@@ -0,0 +1,6 @@
+
+package com.dastan.githubapp;
+
+public class Feature2 {
+    //This is feature 2 branch
+}
